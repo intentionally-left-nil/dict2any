2015033678501428fcef70d9c7cf1c364eb44a16
Anil Kulkarni <anil@terminal.space>
2024-01-17 12:19:29 -0800

    Implement TypedDict parser

--- dict2any/parsers/dict.py
+++ dict2any/parsers/dict.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict
 from collections.abc import Mapping
 from inspect import isclass
-from typing import Any, get_args, get_origin
+from typing import Any, TypedDict, get_args, get_origin, get_type_hints
 
 from dict2any.jq_path import JqPath
 from dict2any.parsers import Parser, Stage, Subparse
@@ -39,3 +39,30 @@ class DictParser(Parser):
                 for key, val in data.items()
             }
         )
+
+
+class TypedDictParser(Parser):
+    def can_parse(self, *, stage: Stage, path: JqPath, field_type: type):
+        match stage:
+            case Stage.Exact:
+                return isclass(field_type) and TypedDict in getattr(field_type, '__orig_bases__', tuple())
+            case _:
+                return False
+
+    def parse(self, *, path: JqPath, field_type: type, data: Any, subparse: Subparse) -> Any:
+        if not isinstance(data, Mapping):
+            raise ValueError(f"Invalid type: {type(data)}")
+        required_keys: frozenset = getattr(field_type, '__required_keys__', frozenset())
+        if any(key not in data for key in required_keys):
+            raise ValueError(f"Missing required keys: {required_keys - frozenset(data.keys())}")
+
+        kwargs = {}
+        allow_unknown_keys: bool = not getattr(field_type, '__total__', True)
+        optional_keys: frozenset = getattr(field_type, '__optional_keys__', frozenset())
+        type_hints = get_type_hints(field_type)
+        for key, value in data.items():
+            if not allow_unknown_keys and (key not in required_keys) and (key not in optional_keys):
+                raise ValueError(f"Unknown key: {key}")
+            parsed_value = subparse(path=path.child(name=str(key)), field_type=type_hints.get(key, Any), data=value)
+            kwargs[key] = parsed_value
+        return field_type(**kwargs)
