c65b0c00668c6d4a6b57b6f76d616865ad79834f
Anil Kulkarni <6687139+intentionally-left-nil@users.noreply.github.com>
2024-01-16 21:36:15 -0800

    Implement a named tuple parser (#10)
    * Implement a named tuple parser

--- dict2any/parsers/tuple.py
+++ dict2any/parsers/tuple.py
@@ -1,5 +1,6 @@
-from collections.abc import Sequence
-from typing import Any, get_args, get_origin
+from collections.abc import Mapping, Sequence
+from inspect import isclass
+from typing import Any, NamedTuple, get_args, get_origin, get_type_hints
 
 from dict2any.jq_path import JqPath
 from dict2any.parsers.parser import Parser, Stage, Subparse
@@ -28,3 +29,43 @@ class TupleParser(Parser):
         for i in range(len(data)):
             sub_items.append(subparse(path=path.child(index=i), field_type=args[i], data=data[i]))
         return field_type(sub_items)
+
+
+class NamedTupleParser(Parser):
+    def can_parse(self, *, stage: Stage, path: JqPath, field_type: type):
+        match stage:
+            case stage.Exact:
+                return isclass(field_type) and issubclass(field_type, tuple) and hasattr(field_type, '_fields')
+            case _:
+                return False
+
+    def parse(self, *, path: JqPath, field_type: type, data: Any, subparse: Subparse) -> Any:
+        kwargs = {}
+        if not isinstance(data, Sequence) and not isinstance(data, Mapping):
+            raise ValueError(f"Invalid type: {type(data)}")
+
+        sub_types: dict[str, Any] = {}
+        field_names: tuple = getattr(field_type, '_fields', tuple())
+        if NamedTuple in getattr(field_type, '__orig_bases__', tuple()):
+            type_hints = get_type_hints(field_type)
+            sub_types = {}
+            for field_name in field_names:
+                sub_types[field_name] = type_hints.get(field_name, Any)
+        else:
+            sub_types = {field_name: Any for field_name in field_names}
+
+        if len(sub_types) != len(data):
+            raise ValueError(f"Invalid tuple length")
+        if isinstance(data, Sequence):
+            for i, (field_name, sub_type) in enumerate(sub_types.items()):
+                kwargs[field_name] = subparse(path=path.child(index=i), field_type=sub_type, data=data[i])
+            return field_type(**kwargs)
+        else:
+            for i, (field_name, sub_type) in enumerate(sub_types.items()):
+                if field_name not in data:
+                    raise ValueError(f"Missing field {field_name}")
+
+                kwargs[field_name] = subparse(
+                    path=path.child(name=field_name), field_type=sub_type, data=data[field_name]
+                )
+            return field_type(**kwargs)
